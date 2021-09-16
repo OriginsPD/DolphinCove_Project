9f458c80534bce7d253014c8fc2df75de96ecfe5
Angella777 <87457801+Angella777@users.noreply.github.com>
2021-09-16 08:40:46 -0500

    Delete database.php

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-//create connection
-$host='localhost:3306';
-$user='root';
-$password='';
-$db='employee_login';
-$DATABASECONNECTION=mysqli_connect($host,$user,$password,$db);//connect database to server and select database
-
-?>
