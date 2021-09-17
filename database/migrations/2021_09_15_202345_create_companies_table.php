027669b1821cbb0e7a0c9df0290a8c05ca5b5232
Sham Don | Top Coder <77687166+jayhacker-garuda@users.noreply.github.com>
2021-09-17 01:48:36 -0500

    guest-count to guest_count and set nullable.

--- database/migrations/2021_09_15_202345_create_companies_table.php
+++ database/migrations/2021_09_15_202345_create_companies_table.php
@@ -16,7 +16,7 @@ class CreateCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('company_name');
-            $table->integer('guest-count');
+            $table->integer('guest_count')->nullable();
             $table->integer('hotel_id');
             $table->timestamps();
         });
